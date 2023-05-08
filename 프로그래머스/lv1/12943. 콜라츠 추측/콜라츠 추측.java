aea24b5e981ce4fcdcbac0a6635fa2520f66b2bd
RawBrain <95132660+RawBrain@users.noreply.github.com>
2023-05-08 11:03:22 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(long num) {
+       int answer = 0;
+	        while(true){
+                if(num ==1){
+                    break;
+                }
+                
+                if(answer ==500){
+                    answer = -1;
+                    break;
+                }
+                
+	            if(num%2==0){
+	                num = num/2;
+	            }else if(num%2 != 0){
+	                num = num*3+1;
+	            }
+                answer++;
+                
+	        }
+        return answer;
+	}
+}
