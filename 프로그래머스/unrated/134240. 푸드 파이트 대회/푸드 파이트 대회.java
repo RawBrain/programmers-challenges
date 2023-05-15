11e69c34db589ecf9faf6010726351d484d7241d
RawBrain <95132660+RawBrain@users.noreply.github.com>
2023-05-15 20:51:03 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 11.70 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i=food.length-1; i>0; i--){
             int n = food[i]/2;
             for(int j=0; j<n; j++){
-                answer = Integer.toString(i) + answer + Integer.toString(i);
+                answer = i + answer + i;
             }
         }
         return answer;
