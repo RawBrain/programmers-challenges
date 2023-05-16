8bcfd65d08fb5f0092b3a4449b1363d5334c8350
RawBrain <95132660+RawBrain@users.noreply.github.com>
2023-05-16 11:07:06 +0900

    [level 1] Title: 모의고사, Time: 2.82 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] stu1 = {1,2,3,4,5};
+        int[] stu2 = {2,1,2,3,2,4,2,5};
+        int[] stu3 = {3,3,1,1,2,2,4,4,5,5};
+        int[] score = new int[3];
+        List<Integer> winner = new ArrayList<>();
+        for(int i=0; i<answers.length; i++){
+            if(answers[i]==stu1[i%5]){
+                score[0]++; 
+            }
+            if(answers[i]==stu2[i%8]){
+                score[1]++;  
+            }
+            if(answers[i]==stu3[i%10]){
+                score[2]++; 
+            }
+        }
+        for(int i=0; i<3; i++){
+            if(Arrays.stream(score).max().getAsInt()==score[i]){
+                winner.add(i+1);
+            }
+        }
+        int[] answer = winner.stream().mapToInt(i->i).toArray();
+        return answer;
+    }
+}
