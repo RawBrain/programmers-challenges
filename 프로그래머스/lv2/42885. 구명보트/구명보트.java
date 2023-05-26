005ce82701b0ecbd23df6d297ef89a4febc63c69
RawBrain <95132660+RawBrain@users.noreply.github.com>
2023-05-26 14:36:14 +0900

    [level 2] Title: 구명보트, Time: 9.16 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -1,21 +1,15 @@
 import java.util.*;
-import java.util.stream.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Arrays.sort(people);
         int lpoint =0;
         int rpoint =people.length-1;
         
-        while(lpoint != rpoint+1){
-            
-            while(lpoint != rpoint){
-                if(people[rpoint] + people[lpoint]<=limit){
-                    people[rpoint] = people[rpoint] + people[lpoint];
-                    lpoint++;
-                }else{
-                    break;
-                }
+        Arrays.sort(people);
+
+        while(lpoint <= rpoint){
+            if(people[rpoint] + people[lpoint]<=limit){
+                lpoint++;
             }
             rpoint--;
             answer++;
