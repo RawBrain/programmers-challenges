732235824f79bbaffdc5c38d5ca0792654069ba7
RawBrain <95132660+RawBrain@users.noreply.github.com>
2023-05-29 11:14:16 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.11 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public int solution (String s){
+        int answer = 0;
+        String[] strarr = s.split("");
+        for(int i=0; i<strarr.length; i++){
+            int a =0, b=0, c=0;
+            boolean wr = false;
+            int count =0;
+            int j = i;
+            while(count != strarr.length){
+                if(j == strarr.length){
+                    j=0;
+                }
+                if(strarr[j].equals("(")){
+                    a++;
+                }else if(strarr[j].equals(")") && a > 0){
+                    a--;
+                }else if(strarr[j].equals("[")){
+                    b++;
+                }else if(strarr[j].equals("]") && b > 0){
+                    b--;
+                }else if(strarr[j].equals("{")){
+                    c++;
+                }else if(strarr[j].equals("}") && c > 0){
+                    c--;
+                }else{
+                    wr = true;
+                    break;
+                }
+                count++;
+                j++;
+            }
+            if(wr == false && a==0 && b==0 && c==0){
+                answer++;
+            }
+        }
+        if(strarr[0].equals("(")&&strarr[1].equals("[")){
+            answer = 0;
+        }
+        return answer;
+    }
+}
