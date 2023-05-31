4eace3afa45b312abafdb2159787fb5bb8ff1eb4
RawBrain <95132660+RawBrain@users.noreply.github.com>
2023-05-31 19:38:21 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.11 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string){
+        int answer = 0;
+        
+        for(int i=0; i<my_string.length(); i++){
+            try{
+                answer +=Integer.parseInt(my_string.substring(i,i+1));
+            }catch(NumberFormatException ex){}
+        }
+        return answer;
+    }
+}
