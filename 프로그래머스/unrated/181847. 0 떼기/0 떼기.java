56037dd1ee328151829823882680a48fa8c6637b
RawBrain <95132660+RawBrain@users.noreply.github.com>
2023-09-06 17:02:19 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String n_str) {
+        int n =0;
+        for(int i=0; i<n_str.length(); i++){
+            if(n_str.charAt(i)-'0'==0){
+                n++;
+            }else{
+                break;
+            }
+        }
+        return n_str.substring(n,n_str.length());
+    }
+}
