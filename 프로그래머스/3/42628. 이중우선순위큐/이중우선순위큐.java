4c9df392e84fcaec796acda5a2eff4be6f7bbad4
RawBrain <95132660+RawBrain@users.noreply.github.com>
2024-09-01 15:18:48 +0900

    [level 3] Title: 이중우선순위큐, Time: 68.32 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,64 +1,26 @@
 import java.util.*;
 
 class Solution {
-    private DoubleEndedPriorityQueue<Integer> deque;
-
-    public Solution() {
-        this.deque = new DoubleEndedPriorityQueue<>();
-    }
-
     public int[] solution(String[] operations) {
-        for (String str : operations) {
-            if (str.equals("D 1")) {
-                deque.pollMax();
-            } else if (str.equals("D -1")) {
-                deque.pollMin();
-            } else {
-                int num = Integer.parseInt(str.substring(2));
-                deque.add(num);
+        Queue<Integer> minpq = new PriorityQueue<>();
+        Queue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (String operation : operations) {
+            if (operation.startsWith("I ")) {
+                int n = Integer.parseInt(operation.substring(2));
+                minpq.offer(n);
+                maxpq.offer(n);
+            } else if (!minpq.isEmpty() && operation.equals("D -1")) {
+                maxpq.remove(minpq.poll());
+            } else if (!maxpq.isEmpty() && operation.equals("D 1")) {
+                minpq.remove(maxpq.poll());
             }
         }
 
-        if (deque.isEmpty()) {
+        if (minpq.isEmpty() && maxpq.isEmpty()) {
             return new int[]{0, 0};
-        } else {
-            return new int[]{deque.peekMax(), deque.peekMin()};
         }
-    }
-}
 
-class DoubleEndedPriorityQueue<E extends Comparable<E>> {
-    private TreeSet<E> set;
-
-    public DoubleEndedPriorityQueue() {
-        this.set = new TreeSet<>();
-    }
-
-    public void add(E element) {
-        set.add(element);
-    }
-
-    public E peekMin() {
-        return set.isEmpty() ? null : set.first();
-    }
-
-    public E peekMax() {
-        return set.isEmpty() ? null : set.last();
-    }
-
-    public E pollMin() {
-        return set.isEmpty() ? null : set.pollFirst();
-    }
-
-    public E pollMax() {
-        return set.isEmpty() ? null : set.pollLast();
-    }
-
-    public boolean isEmpty() {
-        return set.isEmpty();
-    }
-
-    public int size() {
-        return set.size();
+        return new int[]{maxpq.poll(), minpq.poll()};
     }
 }
